976bbcce0025a943000b77c551e19e60077f955b
Nischal Keshav <nischalkeshav.official@gmail.com>
2025-10-16 20:05:13 -0500

    Stop tracking node_modules

---
+++ frontend/src/Features/Hero/HeroPage.jsx
@@ -0,0 +1,11 @@
+
+function HeroPage() {
+  return (
+    <div>
+      <h1>Hero Section</h1>
+      <p>Welcome to the hero section of the lab.</p>
+    </div>
+  );
+}
+
+export default HeroPage;
---
+++ frontend/src/Features/LabInterface/LabInterfacePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LabInterfacePage() {
+  return (
+    <div>
+      <h1>Lab Interface</h1>
+      <p>Welcome to the lab interface.</p>
+    </div>
+  );
+}
+
+export default LabInterfacePage;
